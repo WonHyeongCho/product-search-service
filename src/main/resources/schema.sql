255d84081fbaf939dbeb98e25b50ce9350e8746b
wonhyeong.cho <tmzkdls7989@gmail.com>
2023-12-17 11:03:17 +0900

    설정 파일 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,24 +1,24 @@
 -- 브랜드
 CREATE TABLE brand (
-                       id INT NOT NULL AUTO_INCREMENT,
-                       name VARCHAR(255) NOT NULL,
-                       PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 -- 카테고리
 CREATE TABLE category (
-                          id INT NOT NULL AUTO_INCREMENT,
-                          name VARCHAR(255) NOT NULL,
-                          PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 -- 상품
 CREATE TABLE product (
-                         id INT NOT NULL AUTO_INCREMENT,
-                         price DECIMAL(10,2) NOT NULL,
-                         brand_id INT NOT NULL,
-                         category_id INT NOT NULL,
-                         PRIMARY KEY (id),
-                         CONSTRAINT fk_brand FOREIGN KEY (brand_id) REFERENCES brand(id),
-                         CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES category(id)
+    id INT NOT NULL AUTO_INCREMENT,
+    price DECIMAL(10,2) NOT NULL,
+    brand_id INT NOT NULL,
+    category_id INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_brand FOREIGN KEY (brand_id) REFERENCES brand(id),
+    CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES category(id)
 );
